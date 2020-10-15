ac7f93c50a09fba34b6020cf9297f1fbb1e82d55
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2020-10-15 19:53:29 +0200

    Add files via upload

---
+++ python/server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import socket
+from datetime import datetime
+from shared import *
+
+
+# Safe handling of resources that require releasing on exceptions.
+with socket.socket (socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+
+    server_socket.bind (('', SERVER_PORT))
+    server_socket.listen (SERVER_SOCKET_BACKLOG)
+
+    print ('Waiting for incoming connection.')
+
+    while True:
+
+        # Safe handling of resources that require releasing on exceptions.
+        with server_socket.accept () [0] as client_socket:
+
+            print ('Accepted an incoming connection.')
+
+
+            data = client_socket.recv (SOCKET_BUFFER_SIZE)
+            if len (data) != 0:
+                now = datetime.now()
+                date_time = now.strftime("%Y-%m-%d %H:%M:%S%Z")
+                client_socket.send("Holla")
+                client_socket.send (date_time)
+                client_socket.send("bannana")
+
+            print ('Client disconnected.')
+
+            # Shutdown precedes close to make sure protocol level shutdown is executed completely.
+            # Close without shutdown may use RST instead of FIN to terminate connection, dropping data that is in flight.
+            #
+            # It is also possible to use shutdown to close input and output streams independently.
+            client_socket.shutdown (socket.SHUT_RDWR)
