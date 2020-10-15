5b2f9778a7f6bf77fabebae5338b05c264b5d44c
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2020-10-15 21:48:05 +0200

    Update client.py

--- python/client.py
+++ python/client.py
@@ -22,15 +22,15 @@ class CurrentServerTime:
 
 
 def server_time (date_time_str):
-    date_time = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
+    date_time = datetime.strptime(date_time_str, DATE_TIME_FORMAT)
     return CurrentServerTime(date_time.year, date_time.month, date_time.day, date_time.hour, date_time.minute, date_time.second)
 
 # Safe handling of resources that require releasing on exceptions.
 with socket.socket (socket.AF_INET, socket.SOCK_STREAM) as client_socket:
 
     client_socket.connect ((SERVER_ADDR, SERVER_PORT))
-    client_socket.send ("H".encode ('utf-8'))
-    recieved = server_time(client_socket.recv (SOCKET_BUFFER_SIZE).decode ('utf-8'))
+    client_socket.send ("H".encode (ENCODING))
+    recieved = server_time(client_socket.recv (SOCKET_BUFFER_SIZE).decode (ENCODING))
     print ('Received message:', recieved.day, recieved.month, recieved.year)
 
     # Shutdown precedes close to make sure protocol level shutdown is executed completely.
